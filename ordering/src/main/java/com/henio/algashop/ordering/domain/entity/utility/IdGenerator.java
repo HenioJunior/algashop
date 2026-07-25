a543a5552a1c00680d54cd55a82075a006fd868c
Henio Junior <henioajunior@gmail.com>
2026-07-25 18:57:17 -0300

    Add `IdGenerator` utility for UUID generation and integrate into `Customer` entity and tests

---
+++ ordering/src/main/java/com/henio/algashop/ordering/domain/entity/utility/IdGenerator.java
@@ -0,0 +1,19 @@
+package com.henio.algashop.ordering.domain.entity.utility;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+
+    public static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator
+            = Generators.timeBasedEpochRandomGenerator();
+
+    private IdGenerator() {
+    }
+
+    public static UUID generateTimeBasedUUID() {
+        return timeBasedEpochRandomGenerator.generate();
+    }
+}
