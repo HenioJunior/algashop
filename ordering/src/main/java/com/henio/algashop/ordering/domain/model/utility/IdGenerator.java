f05e9256ce9939236ae50d9eaf57e6aa3593d832
Henio Junior <henioajunior@gmail.com>
2026-08-09 09:06:48 -0300

    Add time-based UUID generation to `IdGenerator` and integration tests for `Order` persistence

--- ordering/src/main/java/com/henio/algashop/ordering/domain/model/utility/IdGenerator.java
+++ ordering/src/main/java/com/henio/algashop/ordering/domain/model/utility/IdGenerator.java
@@ -1,10 +1,17 @@
 package com.henio.algashop.ordering.domain.model.utility;
 
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
 import io.hypersistence.tsid.TSID;
+
+import java.util.UUID;
 
 public class IdGenerator {
 
     public static final TSID.Factory tsidFactory = TSID.Factory.INSTANCE;
+
+    private static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator
+            = Generators.timeBasedEpochRandomGenerator();
 
     private IdGenerator() {
     }
@@ -16,5 +23,9 @@ public class IdGenerator {
 
     public static TSID generateTSID() {
         return tsidFactory.generate();
+    }
+
+    public static UUID generateTimeBasedUUID() {
+        return timeBasedEpochRandomGenerator.generate();
     }
 }
