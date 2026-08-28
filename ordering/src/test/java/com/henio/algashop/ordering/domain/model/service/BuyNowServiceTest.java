09bd56f66ca6582a400cc5b7274f28b7a88e7f55
Henio Junior <henioajunior@gmail.com>
2026-08-28 17:21:52 -0300

    Expand `BuyNowServiceTest` with edge case scenarios: out-of-stock product and zero quantity; replace `Assertions` with static imports for clarity.

--- ordering/src/test/java/com/henio/algashop/ordering/domain/model/service/BuyNowServiceTest.java
+++ ordering/src/test/java/com/henio/algashop/ordering/domain/model/service/BuyNowServiceTest.java
@@ -2,10 +2,12 @@ package com.henio.algashop.ordering.domain.model.service;
 
 
 import com.henio.algashop.ordering.domain.model.entity.*;
+import com.henio.algashop.ordering.domain.model.exception.ProductOutOfStockException;
 import com.henio.algashop.ordering.domain.model.valueobject.*;
 import com.henio.algashop.ordering.domain.model.valueobject.id.CustomerId;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class BuyNowServiceTest {
 
@@ -29,15 +31,71 @@ class BuyNowServiceTest {
                 paymentMethod
         );
 
-        Assertions.assertThat(order.customerId()).isEqualTo(customerId);
-        Assertions.assertThat(order.billing()).isEqualTo(billing);
-        Assertions.assertThat(order.shipping()).isEqualTo(shipping);
-        Assertions.assertThat(order.isPlaced()).isTrue();
-        Assertions.assertThat(order.items()).hasSize(1);
-        Assertions.assertThat(order.totalItems()).isEqualTo(new Quantity(2));
-        Assertions.assertThat(order.totalAmount()).isEqualTo(product.price()
+        assertThat(order.customerId()).isEqualTo(customerId);
+        assertThat(order.billing()).isEqualTo(billing);
+        assertThat(order.shipping()).isEqualTo(shipping);
+        assertThat(order.isPlaced()).isTrue();
+        assertThat(order.items()).hasSize(1);
+        assertThat(order.totalItems()).isEqualTo(new Quantity(2));
+        assertThat(order.totalAmount()).isEqualTo(product.price()
                 .multiply(new Quantity(2))
                 .add(new Money(shipping.cost().value())));
-        Assertions.assertThat(order.paymentMethod()).isEqualTo(paymentMethod);
+        assertThat(order.paymentMethod()).isEqualTo(paymentMethod);
+        assertThat(order.items())
+                .extracting(
+                        OrderItem::productName,
+                        OrderItem::price,
+                        OrderItem::quantity
+                )
+                .containsExactlyInAnyOrder(
+                        tuple(
+                                new ProductName("Notebook X11"),
+                                new Money("3000"),
+                                new Quantity(2)
+                        )
+                );
+    }
+
+    @Test
+    void shouldNotBuyNowWhenProductIsOutOfStock() {
+        Product product = ProductTestDataBuilder
+                .aProductUnavailable()
+                .build();
+
+        CustomerId customerId = CustomerId.generate();
+        Billing billing = BillingTestDataBuilder.aBilling().build();
+        Shipping shipping = ShippingTestDataBuilder.aShipping().build();
+        Quantity quantity = new Quantity(1);
+        PaymentMethod paymentMethod = PaymentMethod.CREDIT_CARD;
+
+        assertThatExceptionOfType(ProductOutOfStockException.class)
+                .isThrownBy(() -> buyNowService.buyNow(
+                        product,
+                        customerId,
+                        billing,
+                        shipping,
+                        quantity,
+                        paymentMethod
+                ));
+    }
+
+    @Test
+    void shouldNotBuyNowWithZeroQuantity() {
+        Product product = ProductTestDataBuilder.aProduct().build();
+        CustomerId customerId = CustomerId.generate();
+        Billing billing = BillingTestDataBuilder.aBilling().build();
+        Shipping shipping = ShippingTestDataBuilder.aShipping().build();
+        Quantity quantity = new Quantity(0);
+        PaymentMethod paymentMethod = PaymentMethod.CREDIT_CARD;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> buyNowService.buyNow(
+                        product,
+                        customerId,
+                        billing,
+                        shipping,
+                        quantity,
+                        paymentMethod
+                ));
     }
 }
