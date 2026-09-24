f60ceb1423942a7f68c5dbb6f609de325d3df902
Henio Junior <henioajunior@gmail.com>
2026-09-24 15:02:45 -0300

    Remove `CustomerPersistenceEntityQueries` and its implementation; no longer needed for customer query logic.

--- ordering/src/main/java/com/henio/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityQueries.java
+++
@@ -1,9 +0,0 @@
-package com.henio.algashop.ordering.infrastructure.persistence.customer;
-
-import com.henio.algashop.ordering.application.customer.query.CustomerOutput;
-
-import java.util.Optional;
-
-public interface CustomerPersistenceEntityQueries {
-    Optional<CustomerOutput> findByIdAsOutput(long id);
-}
